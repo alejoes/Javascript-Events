f12eb764d6094e0dda43ba29e102e1a73712172c
alejoes <escalante.alejox@gmail.com>
2019-10-22 12:26:18 -0400

    optional 1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,45 +1,20 @@
-// // from data.js
-// var tableData = data;
 
-// // YOUR CODE HERE!
-
-
-
-// inputDate.on("change", function() {
-//     var inputDate = d3.event.target.value;
-//     var tbody = d3.select("tbody");
-
-//     data.forEach(function(alienReport) {
-//         if (alienReport.date===inputDate) {
-//             var row = tbody.append("tr");
-//             Object.entries(alienReport).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//             });
-        
-//         };
-//     });
-// });
 var date = d3.select("#datetime"); 
 var button = d3.select("#filter-btn");
 button.on("click",function(){
     document.getElementById("prba").innerHTML = "";
-    
-    date.on("change",function(){
-        var newDate = d3.event.target.value;
-        var tbody = d3.select("tbody");
-        data.forEach(function(alienReport) {
-            if (alienReport.datetime===newDate) {
-                console.log(alienReport);
-                var row = tbody.append("tr");
-                Object.entries(alienReport).forEach(function([key, value]) {
-                var cell = row.append("td");
-                cell.text(value);
-                });
-            } 
-        })
-    })
-
+    var newDate = datetime.value;
+    var tbody = d3.select("tbody");
+    data.forEach(function(alienReport) {
+        if (alienReport.datetime===newDate) {
+            console.log(alienReport);
+            var row = tbody.append("tr");
+            Object.entries(alienReport).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+            });
+        } 
+    }) 
 })
 
 
