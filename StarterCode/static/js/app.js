6309d8639a0dcc47cc7a977f5788b1c02f36de11
alejoes <escalante.alejox@gmail.com>
2019-10-22 10:26:01 -0400

    primer

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,53 @@
+// // from data.js
+// var tableData = data;
+
+// // YOUR CODE HERE!
+
+
+
+// inputDate.on("change", function() {
+//     var inputDate = d3.event.target.value;
+//     var tbody = d3.select("tbody");
+
+//     data.forEach(function(alienReport) {
+//         if (alienReport.date===inputDate) {
+//             var row = tbody.append("tr");
+//             Object.entries(alienReport).forEach(function([key, value]) {
+//             var cell = row.append("td");
+//             cell.text(value);
+//             });
+        
+//         };
+//     });
+// });
+var date = d3.select("#datetime"); 
+var button = d3.select("#filter-btn");
+button.on("click",function(){
+    document.getElementById("prba").innerHTML = "";
+    
+    date.on("change",function(){
+        var newDate = d3.event.target.value;
+        var tbody = d3.select("tbody");
+        data.forEach(function(alienReport) {
+            if (alienReport.datetime===newDate) {
+                console.log(alienReport);
+                var row = tbody.append("tr");
+                Object.entries(alienReport).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+                });
+            } 
+        })
+    })
+
+})
+
+
+
+//date.addEventListener("input",date_input);
+
+// document.getElementById("datetime").addEventListener("input", function(){
+//     var inputDate = date.value;
+//     console.log(inputDate);
+//     return inputDate;
+// })
