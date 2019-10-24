0d15f6ea62b0af7ecbc302b502cbd57f72c9b0a3
alejoes <escalante.alejox@gmail.com>
2019-10-24 12:05:23 -0400

    final

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,7 +10,7 @@ button.on("click",function(){
     var shapex = shape.value;
     var tbody = d3.select("tbody");
 
-    if (newDate != "" && cityx === "") {
+    if (newDate != "" && cityx === "" && statex === "" && countryx === "" && shapex === "") {
         data.forEach(function(alienReport) {
             if (alienReport.datetime===newDate) {
                 var row = tbody.append("tr");
@@ -20,7 +20,7 @@ button.on("click",function(){
                 });
             } 
         })
-    } else if (newDate === "" && cityx != "") {
+    } else if (newDate === "" && cityx != "" && statex === "" && countryx === "" && shapex === "") {
         data.forEach(function(alienReport) {
             if (alienReport.city===cityx) {
                 var row = tbody.append("tr");
@@ -30,9 +30,39 @@ button.on("click",function(){
                 });
             } 
         })
+    } else if (newDate === "" && cityx === "" && statex != "" && countryx === "" && shapex === "") {
+        data.forEach(function(alienReport) {
+            if (alienReport.state===statex) {
+                var row = tbody.append("tr");
+                Object.entries(alienReport).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+                });
+            } 
+        })
+    } else if (newDate === "" && cityx === "" && statex === "" && countryx != "" && shapex === "") {
+        data.forEach(function(alienReport) {
+            if (alienReport.country===countryx) {
+                var row = tbody.append("tr");
+                Object.entries(alienReport).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+                });
+            } 
+        })
+    } else if (newDate === "" && cityx === "" && statex === "" && countryx === "" && shapex != "") {
+        data.forEach(function(alienReport) {
+            if (alienReport.shape===shapex) {
+                var row = tbody.append("tr");
+                Object.entries(alienReport).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+                });
+            } 
+        })
     } else {
         data.forEach(function(alienReport) {
-            if (alienReport.datetime===newDate && alienReport.city===cityx) {
+            if (alienReport.datetime===newDate && alienReport.city===cityx && statex === "" && countryx === "" && shapex === "") {
                 var row = tbody.append("tr");
                 Object.entries(alienReport).forEach(function([key, value]) {
                 var cell = row.append("td");
